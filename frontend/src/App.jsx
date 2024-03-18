90cf20a4dcb5046338d94b1f4d97eec739c0f4cd
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-03-18 11:20:55 +0530

    style:added user profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { Button } from "@chakra-ui/react"
 import { Container } from '@chakra-ui/react'
-import { Route, Router, Routes } from "react-router-dom"
+import { Route,  Routes } from "react-router-dom"
 import User from "./pages/User"
 import Post from "./pages/Post"
 import Header from "./components/Header"
+import UserHeader from './components/UserHeader'
 
 const App = () => {
   return (
-    <Container maxW={"620px"} border={"1px solid red"}>
+    <Container maxW={"620px"}>
       <Header/>
       <Routes>
-        <Route path="/:username" element={<User/>} />
+        <Route path="/:username" element={<UserHeader/>} />
         <Route path="/:username/post/:pId" element={<Post/>} />
 
       </Routes>
---
+++ frontend/src/components/UserHeader.jsx
@@ -0,0 +1,115 @@
+import { Avatar } from "@chakra-ui/avatar";
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/layout";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+import { Portal } from "@chakra-ui/portal";
+import { useColorMode, useToast } from "@chakra-ui/react";
+import { BsInstagram } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+
+const UserHeader = () => {
+  const toast = useToast();
+
+  const copyURL = () => {
+    const currentURL = window.location.href;
+    navigator.clipboard.writeText(currentURL).then(() => {
+      toast({
+        position: "top-right",
+        description: "Profile link copied.",
+        duration: 3000,
+      });
+    });
+  };
+
+  return (
+    <VStack gap={3} alignItems={"start"}>
+      <Flex justifyContent={"space-between"} w={"full"}>
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Kirtikumar
+          </Text>
+          <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"sm"}>kirtikumarKavande</Text>
+
+            <Text
+              fontSize={"xs"}
+              bg={"gray.dark"}
+              
+              color={"gray.light"}
+              p={1}
+              borderRadius={"full"}
+            >
+              threads.net
+            </Text>
+          </Flex>
+        </Box>
+        <Box>
+          <Avatar
+            name={"kirtikumar"}
+            src={"../../public/img/zuck-avatar.png"}
+            size={{
+              base: "md",
+              md: "xl",
+            }}
+          />
+        </Box>
+      </Flex>
+
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa itaque
+        voluptatibus quaerat cumque minima quos ipsam officia pariatur mollitia
+        veniam!
+      </Text>
+
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>1.4k followers</Text>
+          <Box w="1" h="1" bg={"gray.light"} borderRadius={"full"}></Box>
+          <Link color={"gray.light"}>instagram.com</Link>
+        </Flex>
+        <Flex>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO size={24} cursor={"pointer"} />
+              </MenuButton>
+              <Portal>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem bg={"gray.dark"} color={"white"} onClick={copyURL}>
+                    Copy link
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
+      </Flex>
+
+      <Flex w={"full"}>
+        <Flex
+          flex={1}
+          borderBottom={"1.5px solid white"}
+          justifyContent={"center"}
+          pb="3"
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}> Threads</Text>
+        </Flex>
+        <Flex
+          flex={1}
+          borderBottom={"1px solid gray"}
+          justifyContent={"center"}
+          color={"gray.light"}
+          pb="3"
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}> Replies</Text>
+        </Flex>
+      </Flex>
+    </VStack>
+  );
+};
+
+export default UserHeader;
