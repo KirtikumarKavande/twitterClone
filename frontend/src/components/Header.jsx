71c72381237cf90d10e66cbcdbb9a4a69a7d1b2f
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-03-23 09:50:30 +0530

    feat:added logout  frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,22 @@
-import { Flex, useColorMode } from "@chakra-ui/react";
 import { MdLightMode } from "react-icons/md";
+
+
+import { Button, Flex, Link, useColorMode } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/user.atom";
+import { RxAvatar } from "react-icons/rx";
+import { Link as RouterLink } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import useLogout from "../hooks/useLogout";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import { MdOutlineSettings } from "react-icons/md";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+	const user = useRecoilValue(userAtom);
+	const logout = useLogout();
+
+
   return (
     <Flex onClick={toggleColorMode} justifyContent={"center"} mt={6} mb={12}>
       {colorMode === "light" ? (
@@ -10,6 +24,24 @@ const Header = () => {
       ) : (
         <MdLightMode size={25} />
       )}
+
+{user && (
+				<Flex alignItems={"center"} gap={4}>
+					<Link as={RouterLink} to={`/${user.username}`}>
+						<RxAvatar size={24} />
+					</Link>
+					<Link as={RouterLink} to={`/chat`}>
+						<BsFillChatQuoteFill size={20} />
+					</Link>
+					<Link as={RouterLink} to={`/settings`}>
+						<MdOutlineSettings size={20} />
+					</Link>
+					<Button size={"xs"} onClick={logout}>
+						<FiLogOut size={20} />
+					</Button>
+				</Flex>
+			)}
+
     </Flex>
   );
 };
