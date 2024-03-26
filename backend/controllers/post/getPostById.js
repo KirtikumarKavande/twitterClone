b1998c9b40498a2189abfafc86b6bf28e7d2f153
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-03-26 18:31:20 +0530

    feat:getting user's post on click of profile

--- backend/controllers/post/getPostById.js
+++ backend/controllers/post/getPostById.js
@@ -2,9 +2,12 @@ const Post = require("../../models/post.model");
 const ApiResponse = require("../../utils/ApiResponse");
 const asyncHandler = require("../../utils/asyncHandler");
 
-const getPostById=asyncHandler( async(req,res)=>{
-  const post= await Post.findById(req.params.pid)
-res.status(200).json(new ApiResponse(200,{post}," post fetched successfully"))
-})
 
-module.exports =getPostById
+const getPostById = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.pid);
+  res
+    .status(200)
+    .json(new ApiResponse(200, { post }, " post fetched successfully"));
+});
+
+module.exports = getPostById;
--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -4,29 +4,45 @@ import useGetDataFromDB from "../hooks/useGetDataFromDb";
 import { useParams } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
 import { useEffect, useState } from "react";
+import Post from "../components/Post";
+import { Flex, Spinner } from "@chakra-ui/react";
 
 const User = () => {
   const getDataFromDb = useGetDataFromDB();
   const { username } = useParams();
   const showToast = useShowToast();
   const [user, setUser] = useState(null);
+  const [post, setPost] = useState([]);
+  console.log("post", post);
   const [loading, setLoading] = useState(true);
+  const [fetchingPosts, setFetchingPosts] = useState(true);
+
   const getUser = async () => {
     try {
       let data = await getDataFromDb(`user/profile/${username}`);
+      let postOfUser = await getDataFromDb(`post/userspost/${username}`);
+
       if (!data.success) {
         showToast("Error", data.message, "error");
         return;
       }
+
+      if (!postOfUser.success) {
+        showToast("Error", postOfUser.message, "error");
+        return;
+      }
+      setUser(data);
+      setPost(postOfUser.data);
+
       // if (data.isFrozen) {
       // 	setUser(null);
       // 	return;
-      // }
-      setUser(data);
     } catch (error) {
       showToast("Error", error.message, "error");
     } finally {
       setLoading(false);
+      setFetchingPosts(false);
+
     }
   };
 
@@ -36,11 +52,18 @@ const User = () => {
   return (
     <div>
       {user && <UserHeader user={user.data} />}
-      <UserPost />
-      <UserPost />
-      <UserPost />
-      <UserPost />
-      <UserPost />
+      {!fetchingPosts && post.length === 0 && <h1>User has not posts.</h1>}
+
+      {fetchingPosts && (
+				<Flex justifyContent={"center"} my={12}>
+					<Spinner size={"xl"} />
+				</Flex>
+			)}
+
+
+{post.map((post) => (
+				<Post key={post._id} post={post} postedBy={post.postedBy} />
+			))}
     </div>
   );
 };
