dd96917f37fe90857df2c3c385bc1dc2962090ad
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-03-27 08:31:56 +0530

    style:improved responsiveness

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,57 @@
-import { Container } from "@chakra-ui/react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Box, Container } from "@chakra-ui/react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import User from "./pages/User";
 import Post from "./pages/Post";
 import Header from "./components/Header";
 import { useRecoilValue } from "recoil";
 import userAtom from "./atoms/user.atom";
 import AuthPage from "./pages/Auth";
-import Homepage from "./pages/Homepage";
+import HomePage from "./pages/Homepage";
 import UpdateProfilePage from "./pages/UpdateProfilePage";
 import CreatePost from "./components/CreatePost";
 
 const App = () => {
   const user = useRecoilValue(userAtom);
+  const { pathname } = useLocation();
+
   return (
-    <Container maxW={"620px"}>
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={user ? <Homepage /> : <Navigate to="/auth" />}
-        />
-        <Route
-          path="/auth"
-          element={!user ? <AuthPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/update"
-          element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
-        />
+    <Box position={"relative"} w="full">
+      <Container
+        maxW={pathname === "/" ? { base: "620px", md: "900px" } : "620px"}
+      >
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <HomePage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/auth"
+            element={!user ? <AuthPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/update"
+            element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
+          />
 
-        <Route
-          path="/:username"
-          element={
-            user ? (
-              <>
+          <Route
+            path="/:username"
+            element={
+              user ? (
+                <>
+                  <User />
+                  <CreatePost />
+                </>
+              ) : (
                 <User />
-                <CreatePost />
-              </>
-            ) : (
-              <User />
-            )
-          }
-        />
+              )
+            }
+          />
+          <Route path="/:username/post/:pid" element={<Post />} />
+        </Routes>
+      </Container>
+    </Box>
 
-        <Route path="/:username/post/:pid" element={<Post />} />
-      </Routes>
-    </Container>
   );
 };
 
